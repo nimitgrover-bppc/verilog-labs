fd327e71d9f214fe5aedb84c58e03177267f5eda
Nimit Grover <f20250119@pilani.bits-pilani.ac.in>
2026-08-28 12:40:28 +0000

    lab01 task2

--- labs/lab01/task2/ripple_adder.v
+++ labs/lab01/task2/ripple_adder.v
@@ -23,5 +23,9 @@ module ripple_adder(
   wire c1, c2, c3;
 
   // TODO: your four FA_Gate instances go here.
+  FA_Gate FA1(.a(a[0]), .b(b[0]), .cin(cin), .sum(sum[0]), .cout(c1));
+  FA_Gate FA2(.a(a[1]), .b(b[1]), .cin(c1), .sum(sum[1]), .cout(c2));
+  FA_Gate FA3(.a(a[2]), .b(b[2]), .cin(c2), .sum(sum[2]), .cout(c3));
+  FA_Gate FA4(.a(a[3]), .b(b[3]), .cin(c3), .sum(sum[3]), .cout(cout));
 
 endmodule
