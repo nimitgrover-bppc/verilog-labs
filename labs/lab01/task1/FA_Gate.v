8706fbb0578138690645793e71006a6d6cdb0f8d
Nimit Grover <f20250119@pilani.bits-pilani.ac.in>
2026-08-28 12:16:36 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -16,10 +16,10 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor (ps,  a,   b);
   and (pc1, a,   b);
-  xor (sum, cin, ps);
+  xor (ps,  a,   b);
   and (pc2, cin, ps);
+  xor (sum, cin, ps);
   or  (cout, pc1, pc2);
 
 endmodule
