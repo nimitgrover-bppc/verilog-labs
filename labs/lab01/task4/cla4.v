edf9d800ce5631d8733810c67da8044a627daf1f
Nimit Grover <f20250119@pilani.bits-pilani.ac.in>
2026-08-29 21:19:13 +0530

    lab01 task4

--- labs/lab01/task4/cla4.v
+++ labs/lab01/task4/cla4.v
@@ -35,5 +35,44 @@ module cla4(
 
   // TODO: your gate-level P/G, carry, and sum logic goes here.
   // (cout should be connected to c4.) Remember the delay on every gate.
+  wire p0_c;
+  wire p1_g0, p1_p0_c;
+  wire p2_g1, p2_p1_g0, p2_p1_p0_c;
+  wire p3_g2, p3_p2_g1, p3_p2_p1_g0, p3_p2_p1_p0_c;
+
+  xor #(2) (p0, a[0], b[0]);
+  and #(2) (g0, a[0], b[0]);
+  
+  xor #(2) (p1, a[1], b[1]);
+  and #(2) (g1, a[1], b[1]);
+  
+  xor #(2) (p2, a[2], b[2]);
+  and #(2) (g2, a[2], b[2]);
+  
+  xor #(2) (p3, a[3], b[3]);
+  and #(2) (g3, a[3], b[3]);
+
+  and #(2) (p0_c, p0, cin);
+  or  #(2) (c1, g0, p0_c);
+
+  and #(2) (p1_g0, p1, g0);
+  and #(2) (p1_p0_c, p1, p0, cin);
+  or  #(2) (c2, g1, p1_g0, p1_p0_c);
+
+  and #(2) (p2_g1, p2, g1);
+  and #(2) (p2_p1_g0, p2, p1, g0);
+  and #(2) (p2_p1_p0_c, p2, p1, p0, cin);
+  or  #(2) (c3, g2, p2_g1, p2_p1_g0, p2_p1_p0_c);
+
+  and #(2) (p3_g2, p3, g2);
+  and #(2) (p3_p2_g1, p3, p2, g1);
+  and #(2) (p3_p2_p1_g0, p3, p2, p1, g0);
+  and #(2) (p3_p2_p1_p0_c, p3, p2, p1, p0, cin);
+  or  #(2) (cout, g3, p3_g2, p3_p2_g1, p3_p2_p1_g0, p3_p2_p1_p0_c);
+
+  xor #(2) (sum[0], p0, cin);
+  xor #(2) (sum[1], p1, c1);
+  xor #(2) (sum[2], p2, c2);
+  xor #(2) (sum[3], p3, c3);
 
 endmodule
