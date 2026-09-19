372c7e9bfa7bf0493e93e1bc54d3314f53b0ab8f
nimitgrover-bppc <f20250119@pilani.bits-pilani.ac.in>
2026-09-19 13:31:38 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -4,8 +4,14 @@
 module tb;
 
   // TODO: declare the inputs and outputs
+  localparam WIDTH = 8;
+  localparam DEPTH = 4;
+
+  reg  [$clog2(DEPTH)-1:0] sel;
+  wire [WIDTH-1:0] dout;
 
   // TODO: instantiate DUT here
+  lut #(.WIDTH(WIDTH), .DEPTH(DEPTH)) DUT (.sel(sel), .dout(dout));
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
@@ -18,10 +24,16 @@ module tb;
 
   initial begin
     // TODO: apply different input combinations
+    sel = 0; #5;
+    sel = 1; #5;
+    sel = 2; #5;
+    sel = 3; #5;
+
+    $finish;
 
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, "sel = %d | dout = %d", sel, dout);
 
 endmodule
